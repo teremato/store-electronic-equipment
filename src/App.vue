17725a44e27e979ed797389c88e5e9e030be3e3b
teremato <everosha.animation@gmail.com>
2022-10-04 23:21:00 +0500

    rebooting a project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
-    <app-header/>
-    <main-page/>
+    <div></div>
 </template>
 
 <script>
-import mainPage from '@/screens/main/main-page.vue'
-import appHeader from '@/components/layout/header/app-header.vue'
 
-export default {
-    components: {
-        ['app-header']: appHeader,
-        ['main-page']: mainPage
-    }
-}
 </script>
 
 <style lang="scss">
-    @import "@/app.scss";
+    
 </style>
